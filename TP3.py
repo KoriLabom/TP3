0078f7a499ab5bcfb6d2678240feec1949c7a2d5
Mateo Labombarda <ilovepll2005@gmail.com>
2024-10-07 18:41:11 -0300

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -70,3 +70,158 @@ def subMenuGestionarperfil(): #1 MUESTRA SUBMENU: GESTIONAR PERFIL
     print("   1. Editar mis datos personales")
     print("   2. Eliminar mi perfil")
     print("   0. Volver\n")
+def fechaValida(fechastr:str)->bool:
+    try:
+        fechanac = datetime.strptime(fechastr, '%d-%m-%Y').date()
+        fechaactual = date.today()
+        edad = fechaactual.year - fechanac.year - ((fechaactual.month, fechaactual.day) < (fechanac.month, fechanac.day))
+        if 1924 <= fechanac.year <= fechaactual.year and 18 <= edad < 122:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+def opcionesEditar():
+    cls()      
+    print("***** EDITAR DATOS PERSONALES *****\n")
+    print("   ¿Qué desea modificar?")
+    print("   1. Fecha de nacimiento")
+    print("   2. Biografía")
+    print("   3. Hobbies")
+    print("   4. Sexo")
+    print("   0. Volver") 
+def editarDatos(): #EDITAR DATOS
+    opc = "" # asignación interna para obligar al mientras a que entre aunque sea una vez
+    while not (opc=="0"):
+        opcionesEditar()
+        opc = input("\nIngrese una opción: ")
+        while (opc<"0" or opc>"4"):
+            opc = input("Ingreso inválido, ingrese otra opción: ")
+        match opc:
+            case "1": fechaNac()
+            case "2": cambiarBiografia()
+            case "3": cambiarHobbies()
+            case "4": cambiarSexo()
+def subMenuGestionarCandidatos(): #2 MUESTRA SUB MENU: GESTIONAR CANDIDATOS
+    cls()
+    print("***** GESTIONAR CANDIDATOS *****\n")
+    print("   1. Ver candidatos")
+    print("   2. Reportar un candidato")
+    print("   0. Volver.\n")
+def gestCandidatos(): #GESTIONAR CANDIDATOS
+    opc = ""  # asignación interna para obligar al mientras a que entre aunque sea una vez
+    while (opc!="0"):
+        subMenuGestionarCandidatos()        
+        opc = input("\nIngrese una opción: ")
+        while (opc<"0" or opc >"2"):
+            opc = input("Ingreso inválido, ingrese otra opción: ")
+        match opc:
+            case "1": verCandidatos()
+            case "2": reportarCandidato()
+def matcheos():
+    cls()
+    print("***** MATCHEOS *****\n")
+    input("En construcción, presione Enter para volver")
+def calcularEdad(fecha_nacimiento_str):
+    fecha_nacimiento = datetime.strptime(fecha_nacimiento_str, "%d-%m-%Y")
+    fecha_actual = datetime.now()
+    edad = fecha_actual.year - fecha_nacimiento.year
+    if (fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
+        edad -= 1
+    return edad
+def validarUsuario(idR, arrayEstudiantes):
+    global idReportado
+    idActual = 0
+    try:
+        idR_int = int(idR)
+    except ValueError:
+        idR_int = None
+    for estudiante in arrayEstudiantes:
+        if (idR_int == idActual or idR == estudiante[3]) and estudiante[2] == "Activo":
+            idReportado = idActual
+            return True
+        idActual += 1
+    return False
+def menuReportar():
+    cls()
+    mostrarCandidatos()
+    print("\n ***** REPORTAR CANDIDATO ***** \n")
+    print(f"   ID/Nombre del usuario que desea reportar: {idReportado}")
+    print(f"   Motivo del reporte: {motivo}")
+def menuModerador():
+    cls()
+    print("*****  MENÚ MODERADOR *****\n")
+    print("   1. Gestionar Usuarios")
+    print("   2. Gestionar Reportes")
+    print("   3. Reportes Estadisticos")
+    print("   4. Bonus Track 1")
+    print("   5. Bonus Track 2")
+    print("   0. Salir")
+def opcmenuMod():
+    global maxint
+    opc = ""
+    while(opc!= "0"):
+        menuModerador()
+        opc = input("\nIngrese una opción:  ")
+        while (opc<"0" or opc>"5"):
+            opc = input("ingreso inválido, ingrese otra opción: ")
+        match opc:
+            case "1": gestUsuarios()
+            case "2": gestReportes()
+            case "3": repEstadisticos()
+            case "4": Bonustrack1()
+            case "5": Bonustrack2()
+            case "0": maxint = 0
+def gestReportes():
+    opc = ""
+    while(opc!="0"):
+        menuGestRep()
+        opc=input("\nIngrese opción:  ")
+        while(opc<"0" or opc>"1"):
+            opc = input("Ingreso invalido, ingrese otra opción: ")
+        if opc=="1":
+            reportes()
+def menuGestRep():
+    cls()
+    print("***** GESTIONAR REPORTES *****\n")
+    print("   1. Ver reportes")
+    print("   0. Volver")
+def gestUsuarios():
+    opc = ""
+    while(opc!= "0"):
+        menuGestUsuarios()
+        opc = input("\nIngrese opción:  ")
+        while(opc<"0" or opc>"1"):
+            opc = input("Ingreso invalido, ingrese otra opción: ")
+        match opc:
+            case "1": desactivarUsua()
+def menuGestUsuarios():
+    cls()
+    print("***** GESTIONAR USUARIOS *****\n")
+    print("   1. Desactivar usuario")
+    print("   0. Volver")
+def repEstadisticos():
+    cls()
+    print("***** REPORTES ESTADISTICOS *****\n")
+    input("En construcción, presione Enter para volver")
+def inicio():
+    global salir
+    salir=""
+    while salir!="0":
+        cls()
+        print("***** MENU DE INICIO *****\n")
+        print("1. Login")
+        print("2. Registrarse")
+        print("0. Salir del programa")
+        salir = input("\nIngrese una opción: ")
+        while (salir<"0" or salir>"2"):
+            cls()
+            print("1. Login")
+            print("2. Registrarse")
+            print("0. Salir del programa")
+            salir = input("\nIngreso inválido, ingrese otra opción: ")
+        match salir:
+                case "1": login()                      
+                case "2": registro()                               
+                case "0": finPrograma()
+inicio()
